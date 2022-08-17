4c4669bcfdf19583a71d5c48b49b36e513b64dda
ChocolatCat <chocolatcat@protonmail.com>
2022-08-17 14:55:27 -0400

    Removed ID from items

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -5,7 +5,6 @@ const ExpenseListItem = ({id, description, amount, createdAt, note}) => {
     return (
         <div>
             <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-            <h5>{id}</h5>
             <p>{amount} - {createdAt}</p>
             <small>{note}</small>
         </div>
